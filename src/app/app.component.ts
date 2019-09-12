12ab1c05b8d2c23c8615799fcdec4f34e6a5e48f
The Chnow <chnow31@gmail.com>
2019-09-12 22:23:00 +0200

    begin blocs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scout-planning';
+export class AppComponent implements OnInit{
+
+  blocs: any[] = []
+
+  ngOnInit(): void {
+    this.blocs = [
+      {
+        label: "",
+        value: 0.1
+      },
+      {
+        label: "",
+        value: 0.5
+      },
+      {
+        label: "",
+        value: 1
+      },
+      {
+        label: "",
+        value: 2
+      }
+    ];
+    this.blocs.forEach(bloc => {
+      bloc.label = this.time_convert(bloc.value)
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.blocs, event.previousIndex, event.currentIndex);
+  }
+
+  time_convert(num) {
+    
+  }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-    imports: 
-    [
-        MatButtonModule,
-        MatCheckboxModule,
-        MatSidenavModule
-    ],
-    exports: 
-    [
-        MatButtonModule,
-        MatCheckboxModule,
-        MatSidenavModule
-    ],
+    imports:
+        [
+            MatButtonModule,
+            MatCheckboxModule,
+            MatSidenavModule,
+            DragDropModule
+        ],
+    exports:
+        [
+            MatButtonModule,
+            MatCheckboxModule,
+            MatSidenavModule,
+            DragDropModule
+        ],
 })
 export class MaterialModule { }
