f0e9e66e703c84c701b25b249ace5ea30937f99f
Theophane Girard <girard.theophane@gmail.com>
2019-10-05 11:06:18 +0200

    init materials on pdf preview

--- src/app/pdf/pdf-preview/pdf-preview.component.ts
+++ src/app/pdf/pdf-preview/pdf-preview.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivityService } from 'src/app/planning/activities.service';
 import { ActivityBlock } from 'src/app/models/activity-block';
 import { LANG } from 'src/app/core/lang';
+import { Material } from 'src/app/models/material';
+import { Activity } from 'src/app/models/activity';
 
 @Component({
   selector: 'pdf-preview',
@@ -10,6 +12,7 @@ import { LANG } from 'src/app/core/lang';
 })
 export class PdfPreviewComponent implements OnInit {
   private actB: ActivityBlock[] = []
+  private materials: Material[] = []
   readonly LANG = LANG
   @ViewChild('pdf', { static: false }) public pdf
 
@@ -18,6 +21,42 @@ export class PdfPreviewComponent implements OnInit {
   ngOnInit() {
     this.actBService.$activityBlocks.subscribe(actB => this.actB = actB)
     this.actBService.notifyChanges()
+    this.initMaterials()
+  }
+
+  initMaterials() {
+    let tmpActivities: Activity[] = []
+    let tmpMaterials: Material[] = []
+    let tmpMat: Material
+    let tmpLabel: string = ''
+    let tmpAmount: number = 0
+
+    this.actB.forEach(block => {
+      block.activities.forEach(act => {
+        tmpActivities.push(act)
+      });
+    });
+    tmpActivities.forEach(act => {
+      act.materials.forEach(mat => {
+        tmpMaterials.push(mat)
+      })
+    })
+
+    tmpMaterials.forEach(mat => {
+      if (this.materials.every(mat => mat.label !== mat.label)) {
+        tmpLabel = ''
+        tmpAmount = 0
+        tmpMaterials.filter(tmpMat => mat.label === tmpMat.label).forEach(mat => {
+          tmpLabel = mat.label
+          tmpAmount += mat.amount
+        });
+        tmpMat = {
+          label: tmpLabel,
+          amount: tmpAmount
+        }
+        this.materials.push(tmpMat)
+      }
+    })    
   }
 
   download() {
