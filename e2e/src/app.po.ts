e965152a43fcfe672ece1d591c05b2a1244a865e
Theophane Girard <girard.theophane@gmail.com>
2021-04-11 11:57:00 +0200

    CI

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,7 @@ export class AppPage {
   }
 
   getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return browser.getTitle() as Promise<string>;
+    // return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
 }
