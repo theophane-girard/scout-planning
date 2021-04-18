c55000181fb208c63241e0ce16335640ee6e7e1e
Theophane Girard <girard.theophane@gmail.com>
2021-04-18 12:42:34 +0200

    days number choice

--- src/app/planning/planning/planning.component.ts
+++ src/app/planning/planning/planning.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LANG } from 'src/app/core/lang';
 import { DisplaySettingsService } from '../display-settings.service';
+import { MatDialog } from '@angular/material/dialog';
+import { InitDialog } from 'src/app/dialogs/init-dialog/init-dialog';
+import { ActivityService } from '../activities.service';
 
 @Component({
   selector: 'app-planning',
@@ -12,12 +15,23 @@ export class PlanningComponent implements OnInit {
   showMaterial: boolean
   showActDuration: boolean
 
-  constructor(private displaySettingService: DisplaySettingsService) { }
+  constructor(
+    private displaySettingService: DisplaySettingsService,
+    public dialog: MatDialog,
+    private actBService: ActivityService,
+  ) { }
 
   ngOnInit() {
     this.displaySettingService.$showActDuration.subscribe(value => this.showActDuration = value)
     this.displaySettingService.$showMaterial.subscribe(value => this.showMaterial = value)
     this.displaySettingService.notifyChanges()
+
+    if (this.actBService.getDaysNumber() === 0) {
+      const dialogRef = this.dialog.open(InitDialog, {
+        width: '500px',
+        disableClose: true
+      });
+    }
   }
 
   updateShowMaterial() {
